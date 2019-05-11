26a0382f5a06631b71cca86f618807de539ae665
Morgan Kenyon <mkenyon@fileandserve.com>
2019-05-10 21:22:39 -0500

    Adding Benchmark code for everyone!!!

---
+++ Benchmarker/ListTester.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Benchmarker
+{
+    public class ListTester
+    {
+        public void TestList()
+        {
+            var intList = new List<int>();
+            Console.WriteLine($"count/capacity: {intList.Count}/{intList.Capacity}");
+
+            for(int i = 0; i < 10; i++)
+            {
+                intList.Add(i);
+                Console.WriteLine($"count/capacity: {intList.Count}/{intList.Capacity}");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Benchmarker/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+
+namespace Benchmarker
+{
+    [MemoryDiagnoser]
+    public class Md5VsSha256
+    {
+        private SHA256 sha256 = SHA256.Create();
+        private MD5 md5 = MD5.Create();
+        private byte[] data;
+
+        [Params(1000)]
+        public int N;
+
+        [GlobalSetup]
+        public void Setup()
+        {
+            data = new byte[N];
+            new Random(42).NextBytes(data);
+        }
+
+        [Benchmark]
+        public byte[] Sha256() => sha256.ComputeHash(data);
+
+        [Benchmark]
+        public byte[] Md5() => md5.ComputeHash(data);
+    }
+    
+    [MemoryDiagnoser]
+    public class ListMemoryBenchmarker
+    {
+        
+        [Params(1000, 1000000)]
+        public int N;
+        
+        [Benchmark]
+        public void Array()
+        {
+            var array = new int[N];
+            for(int i = 0; i < N; i++) 
+            {
+                array[i] = i;
+            }
+        }
+        
+        [Benchmark]
+        public void ListA()
+        {
+            var list = new List<int>(N);
+            for(int i = 0; i < N; i++) 
+            {
+                list.Add(i);
+            }
+        }
+        
+        [Benchmark]
+        public void ListB()
+        {
+            var list = new List<int>();
+            for(int i = 0; i < N; i++) 
+            {
+                list.Add(i);
+            }
+            
+        }
+    }
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var summary = BenchmarkRunner.Run<ListMemoryBenchmarker>()
+        }
+    }
+}
